d296d991becdf3bca91e20fc6a9ffb2044e7b286
Ahmed Al-Dolabi <ahmed_dolabi@hotmail.com>
2024-05-21 10:20:07 +0300

    new console application

---
+++ bin/dart_console_openapi.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_openapi/dart_console_openapi.dart' as dart_console_openapi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console_openapi.calculate()}!');
+}
---
+++ test/dart_console_openapi_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_console_openapi/dart_console_openapi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

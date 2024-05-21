ab0be78e93824796b3ee7e826c079b4904a3b79a
Ahmed Al-Dolabi <ahmed_dolabi@hotmail.com>
2024-05-21 11:13:50 +0300

    added deoendecies based on chatgpt

--- bin/dart_console_openapi.dart
+++ bin/dart_console_openapi.dart
@@ -1,5 +1,15 @@
-import 'package:dart_console_openapi/dart_console_openapi.dart' as dart_console_openapi;
+import 'package:dart_console_openapi/dart_console_openapi.dart'
+    as dart_console_openapi;
+import 'package:dio/dio.dart';
+import 'package:openapi/openapi.dart';
 
 void main(List<String> arguments) {
+  var dio = new Dio();
+  var openApi = new Openapi(dio: dio);
+
+  var userApi = openApi.getUserApi();
+  userApi.getUserByName(username: "test").then((value) {
+    print(value);
+  });
   print('Hello world: ${dart_console_openapi.calculate()}!');
 }
